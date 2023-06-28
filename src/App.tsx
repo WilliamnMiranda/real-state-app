b92abab5883bb547f5fa6aaf6855ea91d422028c
william nascimento <williamnnmiranda@gmail.com>
2023-06-28 20:25:03 -0300

    chore: :arrow_up: installed fonts and created initial configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import IntroSlider from "./components/intro";
-
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { Poppins_700Bold } from "@expo-google-fonts/poppins";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { useCallback } from "react";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+    Lato_400Regular,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <StatusBar />
       <IntroSlider />
     </View>
